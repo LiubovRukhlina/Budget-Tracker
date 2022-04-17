0a6e9bf77f2ee3fc184af151023a882da5bc0eca
LR <liuba.rukhlina@gmail.com>
2022-04-18 00:08:27 +0200

    API get all expenses

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,80 +1,98 @@
-import {createContext, useEffect, useReducer} from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { api } from "../services/api";
 
 const calculateTotal = (list) => {
-	return list.reduce((total, item) => {
-		return (total = total + item.cost);
-	}, 0)
-}
+  return list.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+};
 
 const AppReducer = (state, action) => {
-	switch (action.type) {
-		case 'SET_BUDGET': {
-			const newBudget = +action.payload || 0
+  switch (action.type) {
+    case "SET_STATE": {
+      return {
+        ...action.payload,
+      };
+    }
+    case "SET_BUDGET": {
+      const newBudget = +action.payload || 0;
 
-			return {
-				...state,
-				budget: newBudget,
-			};
-		}
-		case 'ADD_EXPENSE': {
-			const newExpenses = [...state.expenses, action.payload]
-			const newTotalExpenses = calculateTotal(newExpenses)
+      return {
+        ...state,
+        budget: newBudget,
+      };
+    }
+    case "ADD_EXPENSE": {
+      const newExpenses = [...state.expenses, action.payload];
+      const newTotalExpenses = calculateTotal(newExpenses);
 
-			return {
-				...state,
-				expenses: [...state.expenses, action.payload],
-				totalExpenses: newTotalExpenses,
-			};
-		}
-		case 'DELETE_EXPENSE': {
-			const newExpenses = state.expenses.filter(
-				(expense) => expense.id !== action.payload
-			)
-			const newTotalExpenses = calculateTotal(newExpenses)
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+        totalExpenses: newTotalExpenses,
+      };
+    }
+    case "DELETE_EXPENSE": {
+      const newExpenses = state.expenses.filter(
+        (expense) => expense.id !== action.payload
+      );
+      const newTotalExpenses = calculateTotal(newExpenses);
 
-			return {
-				...state,
-				expenses: newExpenses,
-				totalExpenses: newTotalExpenses,
-			};
-		}
-		default:
-			return state;
-	}
+      return {
+        ...state,
+        expenses: newExpenses,
+        totalExpenses: newTotalExpenses,
+      };
+    }
+    default:
+      return state;
+  }
 };
 
 const initialState = {
-	budget: 10000,
-	expenses: [
-		{ id: 12, name: 'cat', cost: 100 },
-		{ id: 13, name: 'cucumber', cost: 40 },
-		{ id: 14, name: 'laptop', cost: 1150 },
-	],
-	totalExpenses: 1290,
+  budget: 0,
+  expenses: [],
+  totalExpenses: 0,
 };
 
-export const initializer = (initialValue = initialState) =>
-	JSON.parse(localStorage.getItem("localAppState")) || initialValue;
+export const initializer = async (initialValue = initialState) => {
+  const response = await api.get("/expenses");
+  const fetchedExpenses = response.data;
+
+  return {
+    budget: 0,
+    expenses: fetchedExpenses,
+    totalExpenses: calculateTotal(fetchedExpenses),
+  };
+};
 
 export const AppContext = createContext();
 
 export const AppProvider = (props) => {
-	const [state, dispatch] = useReducer(AppReducer, initializer());
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-	useEffect(() => {
-		localStorage.setItem("localAppState", JSON.stringify(state));
-	}, [state]);
+  useEffect(() => {
+    const fetchState = async () => {
+      const fetchedState = await initializer();
 
-	return (
-		<AppContext.Provider
-			value={{
-				budget: state.budget,
-				expenses: state.expenses,
-				totalExpenses: state.totalExpenses,
-				dispatch,
-			}}
-		>
-			{props.children}
-		</AppContext.Provider>
-	);
+      dispatch({
+        type: "SET_STATE",
+        payload: fetchedState,
+      });
+    };
+    fetchState();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        budget: state.budget,
+        expenses: state.expenses,
+        totalExpenses: state.totalExpenses,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </AppContext.Provider>
+  );
 };
